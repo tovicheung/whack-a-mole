0bbfa59155a445723427fcb5ea95f45fe05d3be3
tovicheung <discordpto1227@gmail.com>
2023-12-28 19:08:56 +0800

    powerup: timer

--- main.py
+++ main.py
@@ -12,33 +12,71 @@ FPS = 60
 
 pygame.font.init()
 font = pygame.font.SysFont("Comic Sans MS", 20)
-mole_image = pygame.transform.scale(pygame.image.load("mole_final.png"), (80, 80))
 
 # Mole sprite
 
-class Mole(pygame.sprite.Sprite):
-    LIFETIME = 3 * FPS
+class Entity(pygame.sprite.Sprite):
+    # Common behaviour between moles and powerups
+
+    # Constants
+    lifetime = 3 * FPS
+    image: pygame.surface.Surface
 
     def __init__(self): 
         super().__init__()
-        self.image = mole_image
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(0, WIDTH - self.rect.width)
         self.rect.y = random.randint(0, HEIGHT - self.rect.height)
 
-        self.life = type(self).LIFETIME
+        self.life = type(self).lifetime
 
-        moles.add(self)
+        entities.add(self)
 
     def tick(self):
         self.life -= 1
         if self.life <= 0:
             self.kill()
 
+    def check_mouse(self, mouse: tuple[int, int]): ...
+
+class Mole(Entity):
+    image = pygame.transform.scale(pygame.image.load("mole_final.png"), (80, 80))
+
     def check_mouse(self, mouse):
         if self.rect.collidepoint(mouse):
             global score
             score += 1
+            self.kill()
+
+highlight_width = 10
+highlight_duration = FPS / 2
+class PowerUp(Entity):
+    image = pygame.transform.scale(pygame.image.load("powerup_time.png"), (80, 80))
+
+    def tick(self):
+        super().tick()
+        age = self.lifetime - self.life
+        if age < highlight_duration: # highlight exists for 0.5s
+            
+            # Sadly lines don't support transparency
+            # pygame.draw.line(screen, pygame.color.Color(255, 0, 0, round(age / FPS * 255)), (0, self.rect.y), (WIDTH, self.rect.y), 10)
+            # pygame.draw.line(screen, pygame.color.Color(255, 0, 0, round(age / FPS * 255)), (self.rect.x, 0), (self.rect.x, HEIGHT), 10)
+
+            alpha = round(255 - age / highlight_duration * 255)
+
+            vline = pygame.Surface((highlight_width, HEIGHT), pygame.SRCALPHA)
+            vline.fill((255, 0, 0, alpha))
+            screen.blit(vline, (self.rect.centerx - highlight_width / 2, 0))
+            
+            hline = pygame.Surface((WIDTH, highlight_width), pygame.SRCALPHA)
+            hline.fill((255, 0, 0, alpha))
+            screen.blit(hline, (0, self.rect.centery - highlight_width / 2))
+
+
+    def check_mouse(self, mouse):
+        if self.rect.collidepoint(mouse):
+            global timer
+            timer += 3
             self.kill()
 
 # Setup
@@ -47,10 +85,16 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
-moles = pygame.sprite.Group()
+entities = pygame.sprite.Group()
 
 score = 0
 timer = 10
+
+def gen_next_powerup():
+    # Generate powerup in X to Y seconds
+    return timer - random.randint(2, 3)
+
+next_powerup = gen_next_powerup()
 
 # Events
 
@@ -80,20 +124,26 @@ while running:
             if timer <= 0:
                 running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            for mole in moles:
-                mole.check_mouse(event.pos)
+            for entity in entities:
+                entity.check_mouse(event.pos)
 
-    for mole in moles:
-        mole.tick()
+    for entity in entities:
+        entity.tick()
 
-    moles.update() 
-    moles.draw(screen)
-    
+    entities.update() 
+    entities.draw(screen)
+
+    if timer <= next_powerup:
+        next_powerup = gen_next_powerup()
+        PowerUp()
+        
     screen.blit(font.render(f"Score: {score} | Time: {timer}s", False, (0, 0, 0)), (0, 0))
     pygame.draw.circle(screen, (255, 0, 0), pygame.mouse.get_pos(), radius=10) # helpful (?) dot
 
     pygame.display.flip() 
     clock.tick(FPS)
+
+# end screen
 
 text = font.render(f"Time's up! Your score: {score}", False, (0, 0, 0), (255, 255, 255))
 screen.blit(text, (WIDTH / 2 - text.get_width() / 2, HEIGHT / 2 - text.get_height() / 2))
