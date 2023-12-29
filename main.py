d93633417399506a67807d098bfc4724727ae4fa
tovicheung <discordpto1227@gmail.com>
2023-12-29 12:08:03 +0800

    wind!

--- main.py
+++ main.py
@@ -13,15 +13,14 @@ FPS = 60
 pygame.font.init()
 font = pygame.font.SysFont("Comic Sans MS", 20)
 
-def load_small(path: str):
-    return pygame.transform.scale(pygame.image.load(path), (80, 80))
+def load_small(path: str, scale=(80, 80)):
+    return pygame.transform.scale(pygame.image.load(path), scale)
 
-# Mole sprite
+# Sprites
 
 class Entity(pygame.sprite.Sprite):
     # Common behaviour between moles and powerups
 
-    # Constants
     lifetime = 3 * FPS
     image: pygame.surface.Surface
 
@@ -44,7 +43,7 @@ class Entity(pygame.sprite.Sprite):
     def check_mouse(self, mouse: tuple[int, int]): ...
 
 class Mole(Entity):
-    image = load_small("mole_final.png")
+    image = load_small("mole.png")
 
     def die(self):
         global score
@@ -59,42 +58,22 @@ class Mole(Entity):
 
     def update(self):
         super().update()
+
         if pygame.sprite.spritecollideany(self, foxes) is not None:
             Cross(*self.rect.center, "#ff6d00")
             self.die()
 
-highlight_width = 10
-highlight_duration = FPS / 2
+        if pygame.sprite.spritecollideany(self, winds) is not None:
+            Cross(*self.rect.center, "#64b0de")
+            self.die()
 
 class PowerUp(Entity):
-    # image = pygame.transform.scale(pygame.image.load("powerup_time.png"), (80, 80))
-
     def __init__(self, image, onclick):
         self.image = image
         self.onclick = onclick
         super().__init__() # self.image is needed for __init__
 
         Cross(*self.rect.center, (255, 0, 0))
-
-    def wupdate(self):
-        super().update()
-        age = self.lifetime - self.life
-        if age < highlight_duration: # highlight exists for 0.5s
-
-            # Sadly lines don't support transparency
-            # pygame.draw.line(screen, pygame.color.Color(255, 0, 0, round(age / FPS * 255)), (0, self.rect.y), (WIDTH, self.rect.y), 10)
-            # pygame.draw.line(screen, pygame.color.Color(255, 0, 0, round(age / FPS * 255)), (self.rect.x, 0), (self.rect.x, HEIGHT), 10)
-
-            alpha = round(255 - age / highlight_duration * 255)
-
-            vline = pygame.Surface((highlight_width, HEIGHT), pygame.SRCALPHA)
-            vline.fill((255, 0, 0, alpha))
-            buffer.blit(vline, (self.rect.centerx - highlight_width / 2, 0))
-            
-            hline = pygame.Surface((WIDTH, highlight_width), pygame.SRCALPHA)
-            hline.fill((255, 0, 0, alpha))
-            buffer.blit(hline, (0, self.rect.centery - highlight_width / 2))
-
 
     def check_mouse(self, mouse):
         if self.rect.collidepoint(mouse):
@@ -103,7 +82,7 @@ class PowerUp(Entity):
 
 def powerup_time():
     global timer
-    timer += 3
+    timer += 5
 
 def powerup_fox():
     Fox()
@@ -111,16 +90,19 @@ def powerup_fox():
     Fox()
     Fox()
 
+def powerup_wind():
+    Wind()
+
 powerups = [
     (load_small("powerup_time.png"), powerup_time),
-    (load_small("powerup_fox.png"), powerup_fox)
+    (load_small("powerup_fox.png"), powerup_fox),
+    (load_small("powerup_wind.png"), powerup_wind)
 ]
 
 fox_images: list[pygame.surface.Surface] = []
 for x in range(7):
     image = pygame.image.load(f"fox/{x}.png")
     fox_images.append(pygame.transform.scale(image, (image.get_width() // 3, image.get_height() // 3)))
-# fox_images = [pygame.image.load(f"fox/{x}.png") for x in range(7)]
 
 class Fox(pygame.sprite.Sprite):    
     def __init__(self):
@@ -145,6 +127,35 @@ class Fox(pygame.sprite.Sprite):
         self.x += 20
 
         if self.x >= WIDTH:
+            self.kill()
+
+class Wind(pygame.sprite.Sprite):
+    image_original = load_small("powerup_wind.png", (150, 150))
+    duration = FPS * 3
+    turn_by = -30
+
+    def __init__(self):
+        super().__init__()
+        self.image = self.image_original
+        self.rect = self.image.get_rect()
+        self.angle = 0
+        self.life = self.duration
+
+        everything.add(self)
+        winds.add(self)
+
+    def update(self):
+        self.angle += self.turn_by
+        self.angle %= 360
+        self.image = pygame.transform.rotate(self.image_original, self.angle)
+        self.rect = self.image.get_rect()
+        
+        x, y = pygame.mouse.get_pos()
+        self.rect.centerx = x
+        self.rect.centery = y
+        
+        self.life -= 1
+        if self.life <= 0:
             self.kill()
 
 class Cross(pygame.sprite.Sprite):
@@ -181,7 +192,6 @@ class Cross(pygame.sprite.Sprite):
         if self.life <= 0:
             self.kill()
 
-
 # Setup
 
 pygame.init()
@@ -192,6 +202,7 @@ buffer = pygame.surface.Surface((WIDTH, HEIGHT), pygame.SRCALPHA) # for the wobb
 everything = pygame.sprite.Group() # everything that needs to be drawn
 entities = pygame.sprite.Group() # interactive and updates per tick
 foxes = pygame.sprite.Group() # separate as foxes aren't interactive
+winds = pygame.sprite.Group()
 
 score = 0
 timer = 20
@@ -219,7 +230,6 @@ pygame.time.set_timer(FOX_WALK, 50)
 
 Mole() # no need to wait 1 second for first mole
 
-# Fox()
 running = True
 while running: 
     screen.fill(GRASS)
@@ -243,7 +253,6 @@ while running:
                 entity.check_mouse(event.pos)
 
     everything.update()
-    pygame.sprite.Sprite.update
     everything.draw(buffer)
 
     if timer <= next_powerup:
@@ -259,7 +268,7 @@ while running:
     else:
         screen.blit(buffer, (0, 0))
 
-    # Text above everything else
+    # Text not affected by wobble
     screen.blit(font.render(f"Score: {score} | Time: {timer}s", False, (0, 0, 0)), (0, 0))
 
     pygame.display.flip()
@@ -267,13 +276,13 @@ while running:
 
     clock.tick(FPS)
 
-# end screen
+# End screen
 
 text = font.render(f"Time's up! Your score: {score}", False, (0, 0, 0), (255, 255, 255))
 screen.blit(text, (WIDTH / 2 - text.get_width() / 2, HEIGHT / 2 - text.get_height() / 2))
 pygame.display.flip()
 
-# freeze end screen
+# Freeze end screen
 
 running = True
 while running:
